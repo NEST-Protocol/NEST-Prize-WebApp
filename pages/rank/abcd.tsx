ac2e25626d189c6c333f21727da8f4dc49460c6f
tunogya <tunogya@qq.com>
2022-11-23 18:52:05 +0800

    update

--- pages/rank/abcd.tsx
+++ pages/rank/abcd.tsx
@@ -1,10 +1,10 @@
-import {HStack, Spacer, Stack, Text} from "@chakra-ui/react";
+import {Button, HStack, Spacer, Stack, Text} from "@chakra-ui/react";
 import useTelegramWebApp from "../../hooks/useTelegramWebApp";
 import {useCallback, useEffect, useMemo, useState} from "react";
 import axios from "axios";
 
 const Abcd = () => {
-  const {user, isValid, refresh} = useTelegramWebApp()
+  const {user, refresh} = useTelegramWebApp()
   const [data, setData] = useState<{
     integralReward: number,
     total: number,
@@ -52,26 +52,27 @@ const Abcd = () => {
 
   return (
     <Stack w={'full'} maxW={'container.sm'} p={'20px'} spacing={'10px'}>
-      {user && userInfo && (
-        <>
-          <Text fontSize={'20px'} fontWeight={'bold'}>Your Ranking</Text>
-          <HStack p={'10px'} bg={'#ECDDDD'} borderRadius={'10px'} spacing={'20px'}>
-            <Stack minW={'40px'} minH={'40px'} bg={'red'} align={"center"} justify={"center"} borderRadius={'full'}>
-              <Text color={'white'}>1</Text>
-            </Stack>
-            <Stack w={'full'}>
-              <Text fontWeight={'bold'}>{userInfo.wallet}</Text>
-              <HStack w={'full'}>
-                <Text fontSize={'sm'} color={'gray.800'}>Point: {userInfo.total}</Text>
-                <Spacer/>
-                <Text fontSize={'sm'} color={'gray.800'}>Giveaway: {userInfo.integralReward}</Text>
-              </HStack>
-            </Stack>
-          </HStack>
-          <Stack h={'20px'}></Stack>
-        </>
+      <Text fontSize={'20px'} fontWeight={'bold'}>Your Ranking</Text>
+      {user && userInfo ? (
+        <HStack p={'10px'} bg={'#ECDDDD'} borderRadius={'10px'} spacing={'20px'}>
+          <Stack minW={'40px'} minH={'40px'} bg={'red'} align={"center"} justify={"center"} borderRadius={'full'}>
+            <Text color={'white'}>1</Text>
+          </Stack>
+          <Stack w={'full'}>
+            <Text fontWeight={'bold'}>{userInfo.wallet}</Text>
+            <HStack w={'full'}>
+              <Text fontSize={'sm'} color={'gray.800'}>Point: {userInfo.total}</Text>
+              <Spacer/>
+              <Text fontSize={'sm'} color={'gray.800'}>Giveaway: {userInfo.integralReward}</Text>
+            </HStack>
+          </Stack>
+        </HStack>
+      ) : (
+        <Button onClick={refresh}>
+          Refresh
+        </Button>
       )}
-
+      <Stack h={'20px'}></Stack>
       <Text fontSize={'20px'} fontWeight={'bold'}>Ranking</Text>
       {data.length > 0 ? data.slice(0, 50).map((item, index) => (
         <HStack key={item.chat_id} p={'10px'} bg={'#ECDDDD'} borderRadius={'10px'} spacing={'20px'}>
