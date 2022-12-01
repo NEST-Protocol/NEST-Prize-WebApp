6338709730bfb791e04b42f8e50ca02fabe00175
tunogya <tunogya@qq.com>
2022-12-01 11:50:20 +0800

    add balance

---
+++ lib/sql.ts
@@ -0,0 +1,11 @@
+import postgres from 'postgres';
+
+const sql = postgres({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+});
+
+export default sql
--- pages/api/balanceOf.ts
+++ pages/api/balanceOf.ts
@@ -1,10 +1,38 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import sql from "../../lib/sql";
+
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).send({
-    ok: true,
-  });
+  const user_id = req.query.id;
+  if (!user_id) {
+    return
+  }
+  try {
+    const balance = await sql`
+        SELECT balance
+        FROM "user"
+        WHERE id = ${user_id}
+    `
+    if (balance.length === 0) {
+      res.status(404).send({
+        ok: false,
+        error: "User not found"
+      })
+    }
+    res.status(200).send({
+      ok: true,
+      data: {
+        balance: balance[0].balance
+      }
+    })
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({
+      ok: false,
+      error: "Internal server error"
+    })
+  }
 }
