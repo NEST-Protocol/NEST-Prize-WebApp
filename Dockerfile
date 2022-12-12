6ff1d6c64b4e25cc367b99bb7d7718682bcf7815
tunogya <tunogya@qq.com>
2022-12-12 18:24:31 +0800

    update

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-#a sample dockerfile for NEXT.js web app
-FROM node:12.18.3-alpine3.9
-WORKDIR /app
-COPY package.json .
-RUN npm install
-COPY . .
-RUN npm run build
-EXPOSE 3000
-CMD ["npm", "start"]
--- lib/redisClient.ts
+++
@@ -1,9 +0,0 @@
-import {createClient} from 'redis';
-
-const client = createClient({
-  url: process.env.REDIS_URL,
-});
-
-client.on('error', (err: any) => console.log('Redis Client Error', err));
-
-export default client;
