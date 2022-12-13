69ec39316de5fd0bdc7b6c378d665bfd92674f27
tunogya <tunogya@qq.com>
2022-12-13 18:07:40 +0800

    use svg

--- pages/api/share.ts
+++ pages/api/share.ts
@@ -1,6 +1,6 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import QRCode from "qrcode";
-import sharp from "sharp";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -19,13 +19,6 @@ export default async function handler(
 </defs>
 </svg>
   `;
-  // make svg to jpeg, and return it
-  // const jpeg = await sharp(Buffer.from(svg)).jpeg().toBuffer();
-  // res.setHeader("Content-Type", "image/jpeg");
-  // res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
-  // res.end(jpeg);
-
-  // return svg
   res.setHeader("Content-Type", "image/svg+xml");
   res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
   res.end(svg);
