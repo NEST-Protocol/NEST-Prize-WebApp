f55030bb3c1dbd4477cf68de9ea65967bb0d4435
tunogya <tunogya@qq.com>
2023-01-17 15:06:39 +0800

    update

--- bot/index.js
+++ bot/index.js
@@ -130,7 +130,7 @@ Giveaway events, click on NESTFi Events.
         [Markup.button.url('invite', `https://nest-prize-web-app-delta.vercel.app/api/share2?from=${ctx.from.id}`)],
         [Markup.button.callback('Set Twitter', 'inputUserTwitter', user?.value?.twitterName), Markup.button.callback('Set Wallet', 'setUserWallet', user?.value?.wallet)],
         [Markup.button.callback('NESTFi S4 Food Festival', 'NESTFiEvents')],
-        [Markup.button.url('go to futures', 'https://finance.nestprotocol.org/#/futures'), Markup.button.callback('Share my Futures', 'shareMyFutures')],
+        [Markup.button.url('go to futures', 'https://finance.nestprotocol.org/#/futures'), Markup.button.callback('Share my positions', 'shareMyFutures')],
       ])
     })
     
@@ -212,7 +212,7 @@ Giveaway events, click on NESTFi Events.
         [Markup.button.url('invite', `https://nest-prize-web-app-delta.vercel.app/api/share2?from=${ctx.from.id}`)],
         [Markup.button.callback('Set Twitter', 'inputUserTwitter', res?.data?.value?.twitterName), Markup.button.callback('Set Wallet', 'setUserWallet', res?.data?.value?.wallet)],
         [Markup.button.callback('NESTFi S4 Food Festival', 'NESTFiEvents')],
-        [Markup.button.url('go to futures', 'https://finance.nestprotocol.org/#/futures'), Markup.button.callback('Share my Futures', 'shareMyFutures')],
+        [Markup.button.url('go to futures', 'https://finance.nestprotocol.org/#/futures'), Markup.button.callback('Share my positions', 'shareMyFutures')],
       ])
     })
   } catch (e) {
