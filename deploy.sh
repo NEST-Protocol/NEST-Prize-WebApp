197cce655334b93ba37db9d8afbe928fdb89ad07
tunogya <tunogya@qq.com>
2023-02-06 17:04:40 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ cp -r locales bot # copy locales to bot
 cd bot
 zip -r deploy.zip index.js locales
 # deploy to lambda
-#aws lambda update-function-code --function-name nest-prize-bot --zip-file fileb://deploy.zip --region ap-northeast-1
-aws lambda update-function-code --function-name test --zip-file fileb://deploy.zip --region ap-northeast-1
+aws lambda update-function-code --function-name nest-prize-bot --zip-file fileb://deploy.zip --region ap-northeast-1
 # remove deploy.zip
 rm deploy.zip
