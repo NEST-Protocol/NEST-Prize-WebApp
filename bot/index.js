bb7196104a39a2bba22bcf21ea12e5c694484387
tunogya <tunogya@qq.com>
2023-03-01 13:58:47 +0800

    update

--- bot/index.js
+++ bot/index.js
@@ -34,8 +34,6 @@ if (token === undefined) {
 
 const bot = new Telegraf(token)
 
-const botName = "NESTRedEnvelopesBot"
-
 bot.start(async (ctx) => {
   const chatId = ctx.chat.id
   const isBot = ctx.from.is_bot
@@ -144,9 +142,9 @@ bot.start(async (ctx) => {
       disable_web_page_preview: true,
       ...Markup.inlineKeyboard([
         [Markup.button.url(t('invite', lang), `https://nest-prize-web-app-delta.vercel.app/api/share2?from=${user?.value?.wallet?.slice(-8)?.toLowerCase()}`)],
-        [Markup.button.callback('Invitation Info', 'pizza')],
+        [Markup.button.webApp('Invitation Info', `https://nest-prize-web-app-delta.vercel.app/pizza?chatId=${ctx.from.id}`)],
         [Markup.button.callback(t('Set Twitter', lang), 'inputUserTwitter', user?.value?.twitterName), Markup.button.callback(t('Set Wallet', lang), 'setUserWallet', user?.value?.wallet)],
-        [Markup.button.callback(t('NESTFi S5 Food Festival', lang), 'NESTFiEvents')],
+        [Markup.button.callback('NESTFi S5 Food Festival (Ended)', 'NESTFiEvents')],
         [Markup.button.callback('NEST Roundtable Rewards', 'NESTRoundtable')],
         [Markup.button.url(t('go to futures', lang), 'https://finance.nestprotocol.org/#/futures'), Markup.button.callback(t('Share my positions', lang), 'shareMyFutures')],
       ])
@@ -267,10 +265,10 @@ bot.action('menu', async (ctx) => {
     }), {
       disable_web_page_preview: true,
       ...Markup.inlineKeyboard([
-        [Markup.button.url(t('invite',lang), `https://nest-prize-web-app-delta.vercel.app/api/share2?from=${ctx.from.id}`)],
-        [Markup.button.callback('Invitation Info', 'pizza')],
+        [Markup.button.url(t('invite',lang), `https://nest-prize-web-app-delta.vercel.app/api/share2?from=${ctx.update.callback_query.from.id}`)],
+        [Markup.button.webApp('Invitation Info', `https://nest-prize-web-app-delta.vercel.app/pizza?chatId=${ctx.update.callback_query.from.id}`)],
         [Markup.button.callback(t('Set Twitter', lang), 'inputUserTwitter', res?.data?.value?.twitterName), Markup.button.callback(t('Set Wallet',lang), 'setUserWallet', res?.data?.value?.wallet)],
-        [Markup.button.callback(t('NESTFi S5 Food Festival', lang), 'NESTFiEvents')],
+        [Markup.button.callback('NESTFi S5 Food Festival (Ended)', 'NESTFiEvents')],
         [Markup.button.callback(t('NEST Roundtable Rewards', lang), 'NESTRoundtable')],
         [Markup.button.url(t('go to futures', lang), 'https://finance.nestprotocol.org/#/futures'), Markup.button.callback(t('Share my positions',lang), 'shareMyFutures')],
       ])
@@ -341,319 +339,13 @@ bot.action('shareMyFutures', async (ctx) => {
 })
 
 bot.action('NESTFiEvents', async (ctx) => {
-  let lang = ctx.update.callback_query.from.language_code
-  if (!['en', 'ja', 'bn', 'id', 'tr', 'vi', 'ko', 'ru'].includes(lang)) {
-    lang = 'en'
-  }
-  axios({
-    method: 'post',
-    url: `https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`,
-    data: {
-      client_id: `${ctx.update.callback_query.from.id}`,
-      user_id: `${ctx.update.callback_query.from.id}`,
-      events: [{
-        name: 'click',
-        params: {
-          value: 'NESTFiEvents',
-          language_code: lang,
-        },
-      }]
-    }
-  }).catch((e) => console.log(e))
   try {
     await lmt.removeTokens(1)
     await ctx.answerCbQuery()
         .catch((e) => console.log(e))
-    await ctx.editMessageText(t(`Event Introduction\n\n🍔 Hamburger (New user First Order Bonus)\nBonus: 200NEST\n\n🍕 Pizza (Invitation Bonus)\nOngoing Bonus:0.1% of the trading volume (only calculate the leverage of opening quantity X).\n\n🐣 Butter chicken (Volume Bonus)\nBonus:\n5x leverage bonus 5–50 NEST.\n10x leverage bonus 10–100 NEST.\n20x leverage bonus 20–200 NEST.\n\n🍦 Ice cream\nReward: 0.05% of total trading volume as bonus pool, 50% of trading volume ranking, 50% of profit ranking\nDetails：https://nest-protocol.medium.com/s5-nestfi-food-festival-63120836d5ba`, lang), {
-      parse_mode: 'Markdown',
-      disable_web_page_preview: true,
+    await ctx.editMessageText(`S5 food festival is end, settlement is in progress.`, {
       ...Markup.inlineKeyboard([
-        [Markup.button.url(t('🍔 Hamburger', lang), `https://t.me/${botName}?start=149`), Markup.button.callback(t('🍕 Pizza', lang), 'pizza')],
-        [Markup.button.callback(t('🐣 Butter chicken', lang), 'butterChicken'), Markup.button.callback(t('🍨 Ice cream', lang), 'iceCream')],
-        [Markup.button.url(t('Once a day', lang), 'https://t.me/NEST_DAO/1305')],
-        [Markup.button.callback(t('« Back', lang), 'menu')]
-      ])
-    })
-  } catch (e) {
-    console.log(e)
-  }
-})
-
-bot.action('pizza', async (ctx) => {
-  let lang = ctx.update.callback_query.from.language_code
-  if (!['en', 'ja', 'bn', 'id', 'tr', 'vi', 'ko', 'ru'].includes(lang)) {
-    lang = 'en'
-  }
-  axios({
-    method: 'post',
-    url: `https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`,
-    data: {
-      client_id: `${ctx.update.callback_query.from.id}`,
-      user_id: `${ctx.update.callback_query.from.id}`,
-      events: [{
-        name: 'click',
-        params: {
-          value: 'pizza',
-          language_code: lang,
-        },
-      }]
-    }
-  }).catch((e) => console.log(e))
-  await lmt.removeTokens(1)
-  await ctx.answerCbQuery()
-      .catch((e) => console.log(e))
-  await ctx.editMessageText(t(`Commission = 0.1% of the trading volume (only calculate the leverage of opening quantity X).`, lang), {
-    parse_mode: 'Markdown',
-    disable_web_page_preview: true,
-    ...Markup.inlineKeyboard([
-      [Markup.button.webApp('Invitation Info', `https://nest-prize-web-app-delta.vercel.app/pizza?chatId=${ctx.update.callback_query.from.id}`)],
-      [Markup.button.callback(t('« Back', lang), 'NESTFiEvents')],
-    ])
-  })
-})
-
-bot.action('iceCream', async (ctx) => {
-  let lang = ctx.update.callback_query.from.language_code
-  if (!['en', 'ja', 'bn', 'id', 'tr', 'vi', 'ko', 'ru'].includes(lang)) {
-    lang = 'en'
-  }
-  axios({
-    method: 'post',
-    url: `https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`,
-    data: {
-      client_id: `${ctx.update.callback_query.from.id}`,
-      user_id: `${ctx.update.callback_query.from.id}`,
-      events: [{
-        name: 'click',
-        params: {
-          value: 'iceCream',
-          language_code: lang,
-        },
-      }]
-    }
-  }).catch((e) => console.log(e))
-  await lmt.removeTokens(1)
-  await ctx.answerCbQuery()
-  await ctx.editMessageText(t(`🍦 Ice cream\nReward: 0.05% of total trading volume as bonus pool, 50% of trading volume ranking, 50% of profit ranking\n1. Trading Volume Ranking\nConditions: Trading volume must be greater than 100,000 (calculated leverage) to be eligible to participate.\nReward: The top 80 rewards will be awarded every three days according to the trading volume ranking.\n2. Profit Ranking\nConditions: The principal amount of a single trade must be greater than 1000nest (not counting leverage) to be eligible to participate.\nReward: The top 80 rewards will be awarded every three days according to the profit ranking`, lang), {
-    parse_mode: 'Markdown',
-    disable_web_page_preview: true,
-    ...Markup.inlineKeyboard([
-      [Markup.button.webApp(t('Rank', lang), `https://nest-prize-web-app-delta.vercel.app/ice-cream?chatId=${ctx.update.callback_query.from.id}`)],
-      [Markup.button.callback(t('« Back', lang), 'NESTFiEvents')],
-    ])
-  })
-})
-
-bot.action('butterChicken', async (ctx) => {
-  let lang = ctx.update.callback_query.from.language_code
-  if (!['en', 'ja', 'bn', 'id', 'tr', 'vi', 'ko', 'ru'].includes(lang)) {
-    lang = 'en'
-  }
-  axios({
-    method: 'post',
-    url: `https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`,
-    data: {
-      client_id: `${ctx.update.callback_query.from.id}`,
-      user_id: `${ctx.update.callback_query.from.id}`,
-      events: [{
-        name: 'click',
-        params: {
-          value: 'butterChicken',
-          language_code: lang,
-        },
-      }]
-    }
-  }).catch((e) => console.log(e))
-  try {
-    const [ticket5, ticket10, ticket20] = await Promise.all([
-      axios({
-        method: 'get',
-        url: `https://cms.nestfi.net/workbench-api/activity/tickets?chatId=${ctx.update.callback_query.from.id}&type=5`,
-        headers: {
-          'Authorization': `Bearer ${process.env.NEST_API_TOKEN}`,
-        }
-      }),
-      axios({
-        method: 'get',
-        url: `https://cms.nestfi.net/workbench-api/activity/tickets?chatId=${ctx.update.callback_query.from.id}&type=10`,
-        headers: {
-          'Authorization': `Bearer ${process.env.NEST_API_TOKEN}`,
-        }
-      }),
-      axios({
-        method: 'get',
-        url: `https://cms.nestfi.net/workbench-api/activity/tickets?chatId=${ctx.update.callback_query.from.id}&type=20`,
-        headers: {
-          'Authorization': `Bearer ${process.env.NEST_API_TOKEN}`,
-        }
-      })
-    ])
-    const ticket5Count = ticket5.data.data?.tickets || 0
-    const ticket5History = ticket5.data.data?.history || []
-    const ticket10Count = ticket10.data.data?.tickets || 0
-    const ticket10History = ticket10.data.data?.history || []
-    const ticket20Count = ticket20.data.data?.tickets || 0
-    const ticket20History = ticket20.data.data?.history || []
-    
-    await lmt.removeTokens(1)
-    ctx.answerCbQuery()
-    await ctx.editMessageText(t('conditions\n\nButter chicken (Trading bonus)\n\nRequirements:\n1.random bonus for every 800 futures NEST volume accumulated\n2. Order length must be greater than 5 minutes, with leverage\noptions of 5x, 10x, 20x（Unlimited times）\n\nBonus:\n5x leverage bonus 5–50 NEST.\n10x leverage bonus 10–100 NEST.\n20x leverage bonus 20–200 NEST.\n\n5x remaining butter chicken: {{ticket5Count}}\n{{ticket5History}}\n\n10x remaining butter chicken: {{ticket10Count}}\n{{ticket10History}}\n\n20x remaining butter chicken: {{ticket20Count}}\n{{ticket20History}}', lang, {
-      ticket5Count: ticket5Count,
-      ticket5History: ticket5History.map((item) => `${item} NEST`).join('\n'),
-      ticket10Count: ticket10Count,
-      ticket10History: ticket10History.map((item) => `${item} NEST`).join('\n'),
-      ticket20Count: ticket20Count,
-      ticket20History: ticket20History.map((item) => `${item} NEST`).join('\n'),
-    }), {
-      parse_mode: 'Markdown',
-      disable_web_page_preview: true,
-      ...Markup.inlineKeyboard([
-        [Markup.button.callback(t('5x draw', lang), 'draw5x', ticket5Count <= 0)],
-        [Markup.button.callback(t('10x draw', lang), 'draw10x', ticket10Count <= 0)],
-        [Markup.button.callback(t('20x draw',lang), 'draw20x', ticket20Count <= 0)],
-        [Markup.button.callback(t('« Back',lang), 'NESTFiEvents')]
-      ])
-    })
-  } catch (e) {
-    console.log(e)
-    ctx.answerCbQuery(t('Some error occurred.', lang))
-  }
-})
-
-bot.action('draw5x', async (ctx) => {
-  let lang = ctx.update.callback_query.from.language_code
-  if (!['en', 'ja', 'bn', 'id', 'tr', 'vi', 'ko', 'ru'].includes(lang)) {
-    lang = 'en'
-  }
-  axios({
-    method: 'post',
-    url: `https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`,
-    data: {
-      client_id: `${ctx.update.callback_query.from.id}`,
-      user_id: `${ctx.update.callback_query.from.id}`,
-      events: [{
-        name: 'click',
-        params: {
-          value: 'draw5x',
-          language_code: lang,
-        },
-      }]
-    }
-  }).catch((e) => console.log(e))
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `https://cms.nestfi.net/workbench-api/activity/tickets?chatId=${ctx.update.callback_query.from.id}&type=5`,
-      headers: {
-        'Authorization': `Bearer ${process.env.NEST_API_TOKEN}`,
-      }
-    })
-    const ticketCount = res.data.data?.tickets || 0
-    const ticketHistory = res.data.data?.history || []
-    
-    await lmt.removeTokens(1)
-    await ctx.answerCbQuery()
-        .catch((e) => console.log(e))
-    await ctx.editMessageText(`5x remaining butter chicken: ${ticketCount}
-${ticketHistory.map((item) => `${item} NEST`).join('\n')}`, {
-      disable_web_page_preview: true,
-      ...Markup.inlineKeyboard([
-        [Markup.button.callback(t('5x draw', lang), 'draw5x', ticketCount <= 0)],
-        [Markup.button.callback(t('« Back', lang), 'butterChicken')]
-      ])
-    })
-  } catch (e) {
-    console.log(e)
-  }
-})
-
-bot.action('draw10x', async (ctx) => {
-  let lang = ctx.update.callback_query.from.language_code
-  if (!['en', 'ja', 'bn', 'id', 'tr', 'vi', 'ko', 'ru'].includes(lang)) {
-    lang = 'en'
-  }
-  axios({
-    method: 'post',
-    url: `https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`,
-    data: {
-      client_id: `${ctx.update.callback_query.from.id}`,
-      user_id: `${ctx.update.callback_query.from.id}`,
-      events: [{
-        name: 'click',
-        params: {
-          value: 'draw10x',
-          language_code: lang,
-        },
-      }]
-    }
-  }).catch((e) => console.log(e))
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `https://cms.nestfi.net/workbench-api/activity/tickets?chatId=${ctx.update.callback_query.from.id}&type=10`,
-      headers: {
-        'Authorization': `Bearer ${process.env.NEST_API_TOKEN}`,
-      }
-    })
-    const ticketCount = res.data.data?.tickets || 0
-    const ticketHistory = res.data.data?.history || []
-    
-    await lmt.removeTokens(1)
-    await ctx.answerCbQuery()
-        .catch((e) => console.log(e))
-    await ctx.editMessageText(`10x remaining butter chicken: ${ticketCount}
-${ticketHistory.map((item) => `${item} NEST`).join('\n')}`, {
-      disable_web_page_preview: true,
-      ...Markup.inlineKeyboard([
-        [Markup.button.callback(t('10x draw', lang), 'draw10x', ticketCount <= 0)],
-        [Markup.button.callback(t('« Back', lang), 'butterChicken')]
-      ])
-    })
-  } catch (e) {
-    console.log(e)
-  }
-})
-
-bot.action('draw20x', async (ctx) => {
-  let lang = ctx.update.callback_query.from.language_code
-  if (!['en', 'ja', 'bn', 'id', 'tr', 'vi', 'ko', 'ru'].includes(lang)) {
-    lang = 'en'
-  }
-  axios({
-    method: 'post',
-    url: `https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`,
-    data: {
-      client_id: `${ctx.update.callback_query.from.id}`,
-      user_id: `${ctx.update.callback_query.from.id}`,
-      events: [{
-        name: 'click',
-        params: {
-          value: 'draw20x',
-          language_code: lang,
-        },
-      }]
-    }
-  }).catch((e) => console.log(e))
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `https://cms.nestfi.net/workbench-api/activity/tickets?chatId=${ctx.update.callback_query.from.id}&type=20`,
-      headers: {
-        'Authorization': `Bearer ${process.env.NEST_API_TOKEN}`,
-      }
-    })
-    const ticketCount = res.data.data?.tickets || 0
-    const ticketHistory = res.data.data?.history || []
-    
-    await lmt.removeTokens(1)
-    await ctx.answerCbQuery()
-        .catch((e) => console.log(e))
-    await ctx.editMessageText(`20x remaining butter chicken: ${ticketCount}
-${ticketHistory.map((item) => `${item} NEST`).join('\n')}`, {
-      disable_web_page_preview: true,
-      ...Markup.inlineKeyboard([
-        [Markup.button.callback(t('20x draw', lang), 'draw20x', ticketCount <= 0)],
-        [Markup.button.callback(t('« Back', lang), 'butterChicken')]
+        [Markup.button.callback('« Back', 'menu')]
       ])
     })
   } catch (e) {
