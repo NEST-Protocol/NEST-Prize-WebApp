18d7e2bce41f160e9e7c0127ef317f8b99f0b5a0
tunogya <tunogya@qq.com>
2023-03-09 10:53:38 +0800

    delete prize

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,6 @@ export default function Home() {
   return (
     <Stack maxW={'container.sm'} w={'full'} h={'full'} textAlign={"center"}>
       <Text>Welcome to NEST Prize!</Text>
-      <Link href={'/prize?code=66'}>
-        Snatch Prize
-      </Link>
-      <Link href={'/history?code=66'}>
-        History
-      </Link>
     </Stack>
   )
 }
